2a45e32f179d1ec25e4457a52956ea8528f96a13
ZYGA1 <j.zynowski123@gmail.com>
2024-05-02 18:14:17 +0200

    auth

--- api/auth/Auth.js
+++ api/auth/Auth.js
@@ -13,15 +13,55 @@ const verifyKey = (req, res , next) => {
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
         if(err){
             return res.status(401).json({Error: "Nieprawidłowy token JWT"})
-        }
+        } 
         req.user = decoded()
         next()
+    })       
+}
+
+const newKey = (key, user, fn) => {
+    jwt.sign(user, key, {expiresIn: 60 * 60}, (err, token) => {
+        if(err) console.log(err)
+        else fn(token)
     })
-        
 }
 
 router.post('/login', (req, res) => {
-    
+    res.set('x-powered-by', 'zyga')
+    const data = req.body.data
+    console.log(data)
+    const verifyUser = async (login, haslo_hash) => {
+        try{
+            const [result] = await db.query('Select login, haslo_hash from uzytkownicy where login = ?', [login])
+            if(result[0] != undefined){
+                if (login === result[0].login){
+                    const [resHaslo] = await db.query('Select haslo_hash, id from uzytkownicy where login = ?', [login])
+                    if(haslo_hash === resHaslo[0].haslo_hash){
+                        newKey(process.env.JWT_SECRET_KEY, {id: resHaslo[0].id, login: result[0].login}, (token) => {
+                            res.status(200).json({Access: "Granted", jwt_token: token})
+                        })
+                } 
+                else{
+                    res.status(401).json({Access: "Denied", Error: "Wrong password"})
+                }
+                } 
+            }
+            else{
+                res.status(401).json({Access: "Denied", Error: "Wrong login or password"})
+            }
+        }
+        catch(err){
+            console.log(err)
+        }
+
+    }
+
+    if(data){
+        verifyUser(data.login, data.haslo_hash)
+    }
+    else{
+        res.status(400).json({Error: "no data served"})
+    }
 })
 
 router.post('register', (req, res) => {
--- web/src/auth/Login.tsx
+++ web/src/auth/Login.tsx
@@ -1,19 +1,62 @@
-
+import { useEffect, useState } from "react"
 
 export default function Login(){
 
-    const handleClick = () => {
-        
-    }
+    const [login, setLogin] = useState('')
+    const [passwd, setPasswd] = useState('')
+    const [error, setError] = useState('')
+
+
+    useEffect(() => {
+
+    }, [])
 
     return (
         <>
-            <form>
-                <label>Login: </label><br/>
-                <input type="text"/><br />
-                <label>Hasło: </label><br />
-                <input type="text" /><br/>
-                <button onClick={handleClick}>Zaloguj</button>
+            <form onSubmit={(e) => {
+                e.preventDefault()
+
+                if (login.length > 1){
+                    
+                    fetch('http://localhost:9000/login', {
+                        method: "POST",
+                        headers:{
+                            'Content-Type': 'application/json',
+                            'connection': 'keep-alive',
+                            'powered-by': 'ts'
+                        },
+                        body: JSON.stringify({
+                            data:{
+                                login: login,
+                                haslo_hash: passwd
+                            }
+                        }),                        
+                    })
+                    .then(result => result.json())
+                    .then(data => console.log(data))
+                    .catch(err => console.log(err))
+                }
+
+                
+
+            }}>
+                <label>Hasło: </label><br/>
+                <input 
+                    type="text"
+                    value={passwd}
+                    onChange={(e) => {
+                        setPasswd(e.target.value)
+                    }}
+                /><br />
+                <label>Login: </label><br />
+                <input 
+                    type="text" 
+                    value={login}
+                    onChange={(e) => {
+                        setLogin(e.target.value)
+                    }}
+                /><br/>
+                <button type="submit">Zaloguj</button>
             </form>        
         </>
     )
